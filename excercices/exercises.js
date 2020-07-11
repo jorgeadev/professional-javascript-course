a3b97d78bd91212eafff16e620f6b0a2586253ff
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2020-07-11 12:35:39 -0500

    Create exercises.js

---
+++ excercices/exercises.js
@@ -0,0 +1,12 @@
+let number = 90;
+
+if (number % 2 === 0) {
+    console.log("The number is even");
+} else {
+    console.log("The number is odd");
+}
+
+let number_1 = parseInt(prompt());
+let number_2 = parseInt(prompt());
+
+console.log("Suma", number_1 + number_2)
